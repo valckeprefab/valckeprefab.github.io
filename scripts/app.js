e5ec9eeb67cbcc5099e1490f28e75963c28ae14b
valckeprefab <mattias.hemeryck@valcke-prefab.be>
2022-06-02 08:14:46 +0200

    qdsfdqsf

--- scripts/app.js
+++ scripts/app.js
@@ -168,7 +168,7 @@ async function getRecentOdooData() {
     //var debugInfo = "";
     //Get project name
     var regexProjectName = /^[TV]\d+_\w+/;
-    var project = { name: "V8597_VDL" };//await API.project.getProject();
+    var project = await API.project.getProject();//{ name: "V8597_VDL" };//
     //debugInfo = debugInfo.concat("<br />Project name: " + project.name);
     //$(debug).html(debugInfo);
     if (!regexProjectName.test(project.name))
