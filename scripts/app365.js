115e2e804f3ec7c2f167099cea0d375e7ce4d6a0
valckeprefab <mattias.hemeryck@valcke-prefab.be>
2023-08-22 14:38:42 +0200

    sdqf

---
+++ scripts/app365.js
@@ -0,0 +1,19 @@
+window.onload = async function () {
+    setTextByLanguage();
+}
+
+function getUserLang() {
+    var userLang = navigator.language || navigator.userLanguage;
+    return userLang;
+}
+
+function setTextByLanguage() {
+    var userLang = getUserLang();
+    const div = document.getElementById("test");
+    //console.log(key);
+    //console.log(textUi[key]);
+    //console.log(textUi[key][userLang]);
+    if (div != null && div != undefined) {
+        div.textContent = userLang;
+    }
+}
